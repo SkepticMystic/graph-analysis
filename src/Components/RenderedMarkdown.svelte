d2527ebc77a531398f20e9d930726397292ae61d
Ross Keenan <rossk29@gmail.com>
2022-01-31 07:33:35 +0200

    build: :fire: Remove manifest-beta

--- src/Components/RenderedMarkdown.svelte
+++ src/Components/RenderedMarkdown.svelte
@@ -1,25 +1,23 @@
 <script lang="ts">
-  import { onMount } from 'svelte';
+  import { onMount } from 'svelte'
   import { App, MarkdownRenderer } from 'obsidian'
-  export let sentence: string[];
-  export let sourcePath: string;
-  export let app: App;
-  export let line: number;
-  import {
-    jumpToSelection,
-    openOrSwitch,
-  } from 'src/Utility'
+  export let sentence: string[]
+  export let sourcePath: string
+  export let app: App
+  export let line: number
+  import { jumpToSelection, openOrSwitch } from 'src/Utility'
 
-  let renderedSentence = sentence[0] + "==" + sentence[1] + "==" + sentence[2]
+  let renderedSentence = sentence[0] + '==' + sentence[1] + '==' + sentence[2]
   if (sentence.length === 5) {
-    renderedSentence = renderedSentence + "==" + sentence[3] + "==" + sentence[4]
+    renderedSentence =
+      renderedSentence + '==' + sentence[3] + '==' + sentence[4]
   }
 
-  let el: HTMLElement;
+  let el: HTMLElement
   onMount(async () => {
     MarkdownRenderer.renderMarkdown(renderedSentence, el, sourcePath, null)
-    for(let markedEl:HTMLElement of el.getElementsByTagName("mark")) {
-      markedEl.classList.add("CC-mark")
+    for (let markedEl of el.getElementsByTagName('mark')) {
+      markedEl.classList.add('CC-mark')
     }
   })
 </script>
@@ -28,21 +26,14 @@
   class="CC-sentence"
   bind:this={el}
   on:click={async (e) => {
-  await openOrSwitch(app, sourcePath, e)
-  jumpToSelection(
-    app,
-    line,
-    sentence.join('')
-  )
-}}
->
-</div>
-
+    await openOrSwitch(app, sourcePath, e)
+    jumpToSelection(app, line, sentence.join(''))
+  }}
+/>
 
 <style>
-.CC-sentence {
-  padding-left: 40px;
-  color: var(--text-muted);
-}
-
+  .CC-sentence {
+    padding-left: 40px;
+    color: var(--text-muted);
+  }
 </style>
